0ec125c3cf23bfcdbab17c1dc0ed3b6b558790f4
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2020-07-09 16:31:04 -0700

    na

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,5 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import '../css/application.css';
+require("stylesheets/application.scss")
 const images = require.context('../images', true)
