e897ffdca1116c7a4dd351fdaeefd7d335b1a259
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2020-07-16 16:50:13 -0700

    na

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,7 @@ class BookingsController < ApplicationController
 
   def new
     @flight = Flight.find_by_id(params[:flight])
+    puts @flight
     @booking = Booking.new(flight: @flight)
 
     @num_passengers = params[:passengers].to_i
